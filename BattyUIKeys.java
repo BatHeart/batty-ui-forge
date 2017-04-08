feabdba49d32ab332261fe7fb73cfd0d1ff466f0
BatHeart <BatHeartMC@gmail.com>
2017-04-08 10:44:46 +0100

    The code behind version 1.11.2 (002)
    1.11.2 (001) was the first version compatible with Minecraft 1.11.2
    Version 002 fixes a crash when going below y=0 with the Light Level display active.

---
+++ BattyUIKeys.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package batty.ui;
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
+/**
+ * Handles the detection of key-presses that Batty's Coordinates PLUS mod is interested in 
+ * @author BatHeart
+ */
+public class BattyUIKeys {
+
+	public static boolean keyToggleCoords = false;
+	public static boolean keyMoveCoords = false;
+	public static boolean keyCopyCoords = false; 
+	public static boolean keyToggleTimerVis = false;
+	public static boolean keyToggleTimerRun = false;
+	public static boolean keyResetTimer = false;
+	public static boolean keyMoveTimer = false;
+	public static boolean keyToggleInfoVis = false;
+	public static boolean keyMoveInfo = false;
+
+	
+	@SubscribeEvent
+	public void trackKeyInputs(KeyInputEvent event) {
+
+		if (BattyBaseUI.hideunhideCoordskey.isPressed()) {
+			keyToggleCoords = true;
+		} else if (BattyBaseUI.moveCoordScreenPos.isPressed()) {
+			keyMoveCoords = true;
+		} else if (BattyBaseUI.copyCoordsClipboard.isPressed()) {
+			keyCopyCoords = true;			
+        } else if (BattyBaseUI.hideunhideTimerkey.isPressed()) {
+			keyToggleTimerVis = true;
+		} else if (BattyBaseUI.startstopTimerkey.isPressed()) {
+			keyToggleTimerRun = true;
+		} else if (BattyBaseUI.resetTimerkey.isPressed()) {
+			keyResetTimer = true;
+		} else if (BattyBaseUI.moveTimerScreenPos.isPressed()){
+			keyMoveTimer = true;
+        } else if (BattyBaseUI.hideunhideInfokey.isPressed()) {
+			keyToggleInfoVis = true;
+		} else if (BattyBaseUI.moveInfoScreenPos.isPressed()){
+			keyMoveInfo = true;
+        }
+
+	}
+
+
+    public BattyUIKeys() {
+        
+    }
+
+}
